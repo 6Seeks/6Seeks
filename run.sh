27ea2f291594c96e0d34908429a75350764971cc
yangtao <tao.yang97@outlook.com>
2023-08-23 19:11:30 +0800

    old-version 6Seeks

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ i=$(ip link show $1 | grep -o '^[0-9]')
 m=$(ip link show $1 | grep -o -E "(([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2})" | head -n 1)
 g=$(ip -6 neigh show | grep router | grep -o -E "(([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2})" | head -n 1)
 echo "Interface : $1 (ID : $i), Src Address : $src, Mac : $m , Gateway : $g"
+go clean
 go build
-echo "Exec Commnand : sudo ./Prefixer -i $i -s $src -m $m -g $g"
-sudo ./Prefixer -i $i -s $src -m $m -g $g
+sudo ./Prefixer -i $i -s $src -m $m -g $g -n PrefixSet/P1 -b 1000000000 | tee P1_log
+sudo ./Prefixer -i $i -s $src -m $m -g $g -n PrefixSet/P2 -b 1000000000 | tee P2_log
+sudo ./Prefixer -i $i -s $src -m $m -g $g -n PrefixSet/P3 -b 1000000000 | tee P3_log
+sudo ./Prefixer -i $i -s $src -m $m -g $g -n PrefixSet/P4 -b 1000000000 | tee P4_log
+sudo ./Prefixer -i $i -s $src -m $m -g $g -n PrefixSet/P5 -b 1000000000 | tee P5_log
+sudo ./Prefixer -i $i -s $src -m $m -g $g -n PrefixSet/P6 -b 1000000000 | tee P6_log
